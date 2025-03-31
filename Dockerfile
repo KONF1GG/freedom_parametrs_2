5846de5f15be7fa8215388daad42d8c9fdfe0e64
Леонтий Крохалев <konfig@MacBook-Air-de-Leontij.local>
2025-03-31 19:33:01 +0500

    added docker

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,17 @@
 FROM python:3.10-slim
 
+# Установка зависимостей системы
+RUN apt-get update && apt-get install -y gcc python3-dev && rm -rf /var/lib/apt/lists/*
+
 WORKDIR /app
 
-COPY . /app
+# Сначала копируем только requirements.txt
+COPY requirements.txt .
 
+# Устанавливаем зависимости Python
 RUN pip install --no-cache-dir -r requirements.txt
+
+# Затем копируем весь остальной код
+COPY . .
 
 CMD ["python", "pars_failureConfirmationTime_and_additiional_data.py"]
--- pars_failureConfirmationTime_and_additiional_data.py
+++ pars_failureConfirmationTime_and_additiional_data.py
@@ -45,15 +45,15 @@ async def get_all_data_from_date(date: datetime.date):
     formatted_date = yesterday.strftime('%Y%m%d')
     url = f'http://server1c.freedom1.ru/UNF_CRM_WS/hs/Grafana/anydata?query=failureConfirmationTime&dt_dt={formatted_date}'
     
-    urls = []
-    current_date = date
-    while current_date <= yesterday:
-        formatted_date = current_date.strftime('%Y%m%d')
-        urls.append(f'http://server1c.freedom1.ru/UNF_CRM_WS/hs/Grafana/anydata?query=failureConfirmationTime&dt_dt={formatted_date}')
-        current_date += datetime.timedelta(days=1)
+    # urls = []
+    # current_date = date
+    # while current_date <= yesterday:
+    #     formatted_date = current_date.strftime('%Y%m%d')
+    #     urls.append(f'http://server1c.freedom1.ru/UNF_CRM_WS/hs/Grafana/anydata?query=failureConfirmationTime&dt_dt={formatted_date}')
+    #     current_date += datetime.timedelta(days=1)
 
-    # raw_data = await fetch_all_data([url]) 
-    raw_data = await fetch_all_data(urls)
+    raw_data = await fetch_all_data([url]) 
+    # raw_data = await fetch_all_data(urls)
     parsed_data = [json.loads(item) for item in raw_data if item] 
     flattened_data = [record for batch in parsed_data for record in batch] 
     await upload_to_clickhouse_failureConfirmationTime(flattened_data) 
@@ -96,6 +96,7 @@ async def upload_to_clickhouse_failureConfirmationTime(data):
         if rows:
             column_names = ['id', 'name', 'openingDate', 'closingDate', 'confirmationDate', 'importance']
             client.insert('grafana.failureConfirmationTime', rows, column_names=column_names)
+            client.command('OPTIMIZE TABLE grafana.failureConfirmationTime FINAL;')
             logging.info(f"Вставлено {len(rows)} новых записей в grafana.failureConfirmationTime")
         else:
             logging.info("Нет новых записей для вставки в grafana.failureConfirmationTime")
@@ -140,10 +141,11 @@ async def main():
     try:
         start_date = datetime.date(2025, 3, 1)
         await get_all_data_from_date(start_date)
-        # await get_additional_data()
+        await get_additional_data()
         logging.info("Скрипт успешно завершил выполнение")
     except Exception as e:
         logging.error(f"Ошибка во время выполнения скрипта: {e}")
+
         raise
 
 if __name__ == "__main__":
